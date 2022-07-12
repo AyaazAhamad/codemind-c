fd729ba36d0d292734eac4229b92659edb9540cd
AyaazAhamad <100121514+AyaazAhamad@users.noreply.github.com>
2022-07-12 22:21:01 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,c,j,temp;     float s=0,k=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);//1 2 3 4 5 6 2     }     for(i=0;i<n;i++)//i=1<7     {         c=0;//c=0         for(j=0;j<n;j++)//j=0<7         {             if(a[i]==a[j])//a[1]=a[0]             {                 c++;//c=1                 if(i!=j)                 {                 	a[j]=0; 				}             }         }         if(a[i]!=0)         {             printf("%d %d ",a[i],c);         }     } }

--- Element_and_Count.c
+++ Element_and_Count.c
@@ -1,37 +1,31 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],i,j,c,d=0,b[100],sum=0;
+    int n;
     scanf("%d",&n);
+    int a[n],i,c,j,temp;
+    float s=0,k=0;
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&a[i]);//1 2 3 4 5 6 2
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++)//i=1<7
     {
-  //      printf("%d ",a[i]);
-    }
-    
-    for(i=0;i<n;i++)
-    { c=1;
-        for(j=0;j<n;j++)
+        c=0;//c=0
+        for(j=0;j<n;j++)//j=0<7
         {
-            if(i!=j)
+            if(a[i]==a[j])//a[1]=a[0]
             {
-                if(a[i]==a[j])
-                {  a[j]=1000;
-                    c++;
-                }
+                c++;//c=1
+                if(i!=j)
+                {
+                	a[j]=0;
+				}
             }
         }
-        if(a[i]!=1000)
-        { 
+        if(a[i]!=0)
+        {
             printf("%d %d ",a[i],c);
         }
     }
-
-    //   printf("%d",sum);
-    
-
- 
 }
